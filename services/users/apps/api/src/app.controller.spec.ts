1b9579d90d83662bffa128366496bdb6fe9ad211
Valentin Berlin <valentin.berlin@protonmail.com>
2023-12-27 21:00:48 +0100

    add tests for appController

--- services/users/apps/api/src/app.controller.spec.ts
+++ services/users/apps/api/src/app.controller.spec.ts
@@ -14,9 +14,45 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe('throws error', () => {
+    test('when given credentials of unknown user', () => {
+      const unknownUser = {
+        email: 'a',
+        password: 'b',
+      };
+
+      expect(() => appController.loginUser(unknownUser)).toThrow(
+        'Invalid credentials',
+      );
+    });
+
+    test('when given credentials of known user with wrong password', () => {
+      const knownUser = {
+        email: 'me@pm.com',
+        password: 'b',
+      };
+
+      expect(() => appController.loginUser(knownUser)).toThrow(
+        'Invalid credentials',
+      );
+    });
+  });
+
+  describe('returns user', () => {
+    test('when given credentials of known user with correct password', () => {
+      const knownUser = {
+        email: 'me@pm.com',
+        password: 'mememe',
+      };
+
+      const response = appController.loginUser(knownUser);
+
+      expect(response).toBe(
+        JSON.stringify({
+          email: knownUser.email,
+          status: 'JWT is coming soon',
+        }),
+      );
     });
   });
 });
